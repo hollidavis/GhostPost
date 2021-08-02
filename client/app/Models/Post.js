495ba5d40292977d83a8c010a247bd43dc057db0
hollidavis <hollidavis19@gmail.com>
2021-08-02 09:47:50 -0600

    fixed mobile slightly

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -15,27 +15,27 @@ export class Post {
   get Template() {
     return /* HTML */`
     <div class="row my-3 mx-2 bg-light shadow-card">
-            <div class="col-12">
+            <div class="col-md-12">
                 <div class="row pb-2">
                     <div id="vote-count"
-                        class="col-1 d-flex flex-column align-items-center justify-content-center line-right">
+                        class="col-md-1 d-flex flex-column align-items-center justify-content-center line-right">
                         <span class="mdi mdi-arrow-up-bold mdi-36px text-primary" id="upVote"></span>
                         <span id="upVoteCount">${this.upVote}</span>
                         <span id="downVoteCount">${this.downVote}</span>
                         <span class="mdi mdi-arrow-down-bold mdi-36px text-danger" id="downVote"></span>
                     </div>
-                    <div class="col-11">
+                    <div class="col-md-11">
                         <div class="row justify-content-between align-items-center bg-light-gray px-2 mb-2">
                             <h2 class="my-1">${this.title}</h2>
                             <h4 id="delete-button" class="text-danger justify-text-end my-1" onclick="app.postsController.deletePost('${this.id}')"><b>X</b></h4>
                         </div>
                         <div class="row">
-                            <div class="col-4 mx-0">
+                            <div class="col-md-4 mx-0">
                                 <img class = "post-img mb-2" src="${this.img}" alt="${this.location}">
                             </div>
-                            <div class="col-8">
+                            <div class="col-md-8">
                                 <div class="row">
-                                    <div class="col">
+                                    <div class="col-md-12">
                                         <p class="d-flex align-items-center mdi mdi-map-marker mdi-24px mb-0">${this.location}</p>
                                         <div id="rating">
                                             ${this.getRatings}
@@ -48,7 +48,7 @@ export class Post {
                             </div>
                         </div>
                         <div class="row ">
-                            <div class="col-12 d-flex justify-content-between">
+                            <div class="col-md-12 d-flex justify-content-between">
                                 <button type="button" class="btn btn-primary" data-toggle="modal"
                                     data-target="#v">
                                     <span class="mdi mdi-lead-pencil mdi-10px" title="Edit Post"> Edit Post</span>
